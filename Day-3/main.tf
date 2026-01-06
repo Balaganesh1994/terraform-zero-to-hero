1dd25da6b1474e15f6daa89c1d7624c62e412cd6
balaganesh1994 <balaganesh.dasari@gmail.com>
2026-01-06 10:00:38 +0000

    modified instance size

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
   ami_value = "ami-053b0d53c279acc90" # replace this
-  instance_type_value = "t3.micro"
+  instance_type_value = "t3.small"
   subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
 }
